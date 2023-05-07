172dd42c0745004bddbaa834c8e0b2317876e500
FianaranaDev <fianaranadev@gmail.com>
2023-05-07 18:35:57 +0300

    push
    resolution de jsx cast et du return en html

--- TP_S6_P14_Web_design_Mai_2022/resources/js/Pages/General/CDetail.jsx
+++ TP_S6_P14_Web_design_Mai_2022/resources/js/Pages/General/CDetail.jsx
@@ -5,44 +5,41 @@ export default function CDetail({detail}){
 
 
     const tay = (paragraphes) => {
-        let retournena = "";
+        let elementsJSX = [];
         let useDiv = true;
 
         for (let i = 0; i < paragraphes.length; i++) {
             if (useDiv) {
-            retournena += '<div class="d-md-flex post-entry-2 half">\n' +
-                '            <a href="#" class="me-4 thumbnail">\n' +
-                '              <img src="assets/img/post-landscape-2.jpg" alt="" class="img-fluid">\n' +
-                '            </a>\n' +
-                '            <div class="ps-md-5 mt-4 mt-md-0">\n' +
-                '              <div class="post-meta mt-4">About us</div>\n' +
-                '              <h2 class="mb-4 display-4">Company History</h2>\n' +
-                '\n' +
-                '              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, perspiciatis repellat maxime, adipisci non ipsam at itaque rerum vitae, necessitatibus nulla animi expedita cumque provident inventore? Voluptatum in tempora earum deleniti, culpa odit veniam, ea reiciendis sunt ullam temporibus aut!</p>\n' +
-                '              <p>Fugit eaque illum blanditiis, quo exercitationem maiores autem laudantium unde excepturi dolores quasi eos vero harum ipsa quam laborum illo aut facere voluptates aliquam adipisci sapiente beatae ullam. Tempora culpa iusto illum accusantium cum hic quisquam dolor placeat officiis eligendi.</p>\n' +
-                '            </div>\n' +
-                '          </div>\n';
+                elementsJSX.push(
+                    <div className="d-md-flex post-entry-2 half">
+                        <a href="#" className="me-4 thumbnail">
+                            <img src="assets/img/post-landscape-2.jpg" alt="" className="img-fluid" />
+                        </a>
+                        <div className="ps-md-5 mt-4 mt-md-0">
+                            <div className="post-meta mt-4"></div>
+                            <h2 className="mb-4 display-4">{paragraphes[i].titrepara}</h2>
+                            <p>{paragraphes[i].descriptione}</p>
+                        </div>
+                    </div>
+                );
             } else {
-                retournena += '<div class="d-md-flex post-entry-2 half mt-5">\n' +
-                    '            <a href="#" class="me-4 thumbnail order-2">\n' +
-                    '              <img src="assets/img/post-landscape-1.jpg" alt="" class="img-fluid">\n' +
-                    '            </a>\n' +
-                    '            <div class="pe-md-5 mt-4 mt-md-0">\n' +
-                    '              <div class="post-meta mt-4">Mission &amp; Vision</div>\n' +
-                    '              <h2 class="mb-4 display-4">Mission &amp; Vision</h2>\n' +
-                    '\n' +
-                    '              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, perspiciatis repellat maxime, adipisci non ipsam at itaque rerum vitae, necessitatibus nulla animi expedita cumque provident inventore? Voluptatum in tempora earum deleniti, culpa odit veniam, ea reiciendis sunt ullam temporibus aut!</p>\n' +
-                    '              <p>Fugit eaque illum blanditiis, quo exercitationem maiores autem laudantium unde excepturi dolores quasi eos vero harum ipsa quam laborum illo aut facere voluptates aliquam adipisci sapiente beatae ullam. Tempora culpa iusto illum accusantium cum hic quisquam dolor placeat officiis eligendi.</p>\n' +
-                    '            </div>\n' +
-                    '          </div>';
+                elementsJSX.push( <div class="d-md-flex post-entry-2 half mt-5">
+                                <a href="#" class="me-4 thumbnail order-2">
+                                  <img src="assets/img/post-landscape-1.jpg" alt="" class="img-fluid"/>
+                                </a>
+                                <div class="pe-md-5 mt-4 mt-md-0">
+                                  <div class="post-meta mt-4"></div>
+                                  <h2 class="mb-4 display-4">{paragraphes[i].descriptione}</h2>
+                                  <p>{paragraphes[i].descriptione}</p>
+                                </div>
+                              </div> );
             }
             useDiv = !useDiv;
 
 
         }
-        return (
-            <div dangerouslySetInnerHTML={{ __html: retournena }} />
-        );
+        return <>{elementsJSX}</>;
+
     };
 
     return(
@@ -53,14 +50,6 @@ export default function CDetail({detail}){
                  <meta name="keywords" content={detail.keywords}/>
              </Head>
 
-
-         {/*    <div class="single-post">*/}
-         {/*        <div class="post-meta"><span class="date">{detail.snippet}</span> <span class="mx-1">&bullet;</span> <span> {detail.datepublication}</span></div>*/}
-         {/*            <h1 class="mb-5">{detail.titre}</h1>*/}
-
-
-
-         {/*<div>*/}
              <section>
 
              <div className="container" >
@@ -68,7 +57,7 @@ export default function CDetail({detail}){
 
                  <div className="row">
                      <div className="col-lg-12 text-center mb-5">
-                         <h1 className="page-title">About us</h1>
+                         <h1 className="page-title">{detail.titre}</h1>
                      </div>
                  </div>
 
