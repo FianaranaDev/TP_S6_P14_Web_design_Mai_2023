ce948557b7f1529b5a5ac6fd5fb03ca7cbea44f5
FianaranaDev <fianaranadev@gmail.com>
2023-05-09 17:04:09 +0300

    fix

--- TP_S6_P14_Web_design_Mai_2022/resources/js/Layouts/FOL/FooterContenues.jsx
+++ TP_S6_P14_Web_design_Mai_2022/resources/js/Layouts/FOL/FooterContenues.jsx
@@ -12,8 +12,8 @@ export default function FooterContenues({recent}){
                     {recent.map((c) => (
                     <li>
                         <a href="#" className="d-flex align-items-center">
-                            <img src={"/assets/img/"+c.image} alt="" className="img-fluid me-3"></img>
-                            <div>
+                            <img src={"/assets/img/"+c.image} alt="" width="45" height="35" className="img-fluid me-3"></img>
+                            <div>  
                                 <div className="post-meta d-block"><span className="date">{c.snippet}</span> <span
                                     className="mx-1">;</span> <span>{c.datepublication}</span></div>
                                 <span><a href={"/detail/"+c.id+"-"+"contenue"} >{c.titre}</a></span>
