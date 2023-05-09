d71f7c014b778b5a4f57c9579d88f8bf7de40308
FianaranaDev <fianaranadev@gmail.com>
2023-05-09 08:56:31 +0300

    supprim login

--- TP_S6_P14_Web_design_Mai_2022/routes/web.php
+++ TP_S6_P14_Web_design_Mai_2022/routes/web.php
@@ -32,21 +32,21 @@ Route::get('/user', [App\Http\Controllers\UtilisateurController::class, 'create'
 
 //Back office
     //List contenue  Delete Update
-        Route::get('/admin/login',[App\Http\Controllers\ContenueController::class,'log'])->middleware(['auth']);
+        Route::post('/admin/login',[App\Http\Controllers\ContenueController::class,'listDU']);
 
-        Route::get('/admin/con',[App\Http\Controllers\ContenueController::class,'listDU'])->middleware(['auth']);
+        Route::get('/admin/con',[App\Http\Controllers\ContenueController::class,'listDU']);
                     //ajout
-        Route::get('admin/contenue',[App\Http\Controllers\ContenueController::class,'add'])->middleware(['auth']);
+        Route::get('admin/contenue',[App\Http\Controllers\ContenueController::class,'add']);
                     //insert
-        Route::post('admin/insert',[App\Http\Controllers\ContenueController::class,'insert'])->middleware(['auth']);
+        Route::post('admin/insert',[App\Http\Controllers\ContenueController::class,'insert']);
 
                     //delete
-        Route::get('admin/delete/contenue/',[App\Http\Controllers\ContenueController::class,'delete'])->middleware(['auth']);
+        Route::get('admin/delete/contenue/',[App\Http\Controllers\ContenueController::class,'delete']);
 
                 //modifier
-        Route::get('/admin/modifier/contenue',[App\Http\Controllers\ContenueController::class,'update'])->middleware(['auth']);
+        Route::get('/admin/modifier/contenue',[App\Http\Controllers\ContenueController::class,'update']);
 
-//
+
 
 //update
 
